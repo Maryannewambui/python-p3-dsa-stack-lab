9eadfb9d7d18e7dc382a50ca49d0b565fd394e68
Maryannewambui <trimbleleewambui@gmail.com>
2023-12-07 19:41:11 +0300

    error

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,39 @@
 class Stack:
 
     def __init__(self, items = [], limit = 100):
-        pass
+        self.items = items
+        self.limit = limit
 
     def isEmpty(self):
-        pass
-
-    def push(self, item):
-        pass
-
-    def pop(self):
-        pass
-
-    def peek(self):
-        pass
+        return len(self.items) == 0
     
+    def push(self, item):
+        if len(self.items) < self.limit:
+            self.items.append(item)
+        else:
+            raise Exception("Stack is full")
+        
+    def pop(self):
+        if not self.isEmpty():
+            return self.items.pop()
+        else:
+            raise IndexError("Pop from an empty stack")
+        
+    def peek(self):
+        if not self.isEmpty():
+            return self.items[-1]
+        else:
+            return None
+        
     def size(self):
-        pass
-
+        return len(self.items)
+    
     def full(self):
-        pass
-
+        return len(self.items) == self.limit
+    
     def search(self, target):
-        pass
+        if target in self.items:
+            return len(self.items) - self.items.index(target)
+        else:
+            return -1
+
